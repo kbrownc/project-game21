29fd63434e264971dc73a7be268edd8ab63daf1f
Kim Brown <kbrownc341@gmail.com>
2025-01-31 15:08:42 -0500

    Part B of initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { useState } from 'react'
+import { } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Game21</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Game 21</h1>
     </>
   )
 }
